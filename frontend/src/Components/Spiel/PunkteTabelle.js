1c554a64396acd8b6fb23a0e504497d27b05b370
Beniamin Terza <beniaminterza@gmail.com>
2021-03-15 14:19:19 +0100

    fix: table overflow made scrollable

--- frontend/src/Components/Spiel/PunkteTabelle.js
+++ frontend/src/Components/Spiel/PunkteTabelle.js
@@ -2,18 +2,70 @@ import React from "react";
 
 const PunkteTabelle = () => {
     return (
-        <div className="bg-white flex flex-center text-xs text-center rounded-b-st h-64">
-            <table className="mx-auto my-8">
+        <div className="bg-white flex flex-center text-xs text-center rounded-b-st h-64 overflow-auto">
+            <table className="mx-auto my-4">
                 <tr>
                     <th>Team 1</th>
                     <th>Team 2</th>
                 </tr>
                 <tr>
-                    <td>1</td>
+                    <td>2</td>
                     <td></td>
                 </tr>
                 <tr>
-                    <td>1</td>
+                    <td>2</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>2</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>2</td>
                     <td></td>
                 </tr>
                 <tr>

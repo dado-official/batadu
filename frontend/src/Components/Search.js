283c8a38874acca0a4933f02f008ac5b0516df85
Beniamin Terza <beniaminterza@gmail.com>
2021-03-17 10:02:48 +0100

    feat: searchresults hide when click outside

--- frontend/src/Components/Search.js
+++ frontend/src/Components/Search.js
@@ -1,16 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import SearchIcon from "../assets/search-24px.svg";
 import SearchResults from "./SearchResults";
 
 const Search = () => {
     const [search, setSearch] = useState("");
+    const [isOpenSearch, setIsOpenSearch] = useState(false);
 
     function searchHandler(e) {
         setSearch(e.target.value);
     }
 
+    function openSearchHandler() {
+        setIsOpenSearch(true);
+    }
+
+    const searchResultRef = useRef();
+
+    useEffect(() => {
+        let handler = (e) => {
+            if (
+                searchResultRef.current &&
+                !searchResultRef.current.contains(e.target)
+            ) {
+                setIsOpenSearch(false);
+            }
+        };
+
+        document.addEventListener("mousedown", handler);
+        return () => {
+            document.removeEventListener("mousedown", handler);
+        };
+    });
+
     return (
-        <div className="bg-white rounded-st relative py-2 flex items-center">
+        <div
+            className="bg-white rounded-st relative py-2 flex items-center"
+            onClick={openSearchHandler}
+        >
             <img src={SearchIcon} alt="Lupe" className="px-4" />
             <input
                 type="text"
@@ -19,7 +45,9 @@ const Search = () => {
                 onChange={searchHandler}
                 placeholder="Suchen"
             ></input>
-            <SearchResults />
+            <div ref={searchResultRef}>
+                {isOpenSearch && search !== "" ? <SearchResults /> : null}
+            </div>
         </div>
     );
 };

72fac27cbb08843f7146ab4b4d41e276235044c3
Daniel Nagler <danielnagler1507@gmail.com>
2021-03-18 10:05:24 +0100

    edit: create, join, leave Room

---
+++ Backend/server_projekt/GameElements/Room.js
@@ -0,0 +1,74 @@
+class Room{
+    constructor(config){
+        this.configRoom = config;
+        if(this.configRoom.spielerAnzahl === 4){
+            this.roompositionen = [false, false, false, false] // false == frei
+        } else {
+            this.roompositionen = [false, false] // false == frei
+        }
+
+        //first player in room
+        this.configRoom.spielerIDs[0].position = "0";
+        this.roompositionen[0] = true
+        return 0;
+    }
+
+    calcPosition(person){
+        //spielerposition vergeben
+        for (let i = 0; i < this.configRoom.spielerAnzahl; i++) {
+            if(!this.roompositionen[i]){
+                this.roompositionen[i] = true;
+                return i
+            }
+        }
+        return person;
+    }
+
+    addPerson(personalInfo){
+        if(this.configRoom.spielerIDs.length >= this.configRoom.spielerAnzahl){
+            this.configRoom.zuschauerIDs.push(personalInfo)
+        } else {
+            personalInfo.position = this.calcPosition(personalInfo).toString()
+            this.configRoom.spielerIDs.push(personalInfo)
+        }
+        //console.log(this.configRoom.spielerIDs)
+        //console.log(this.configRoom.zuschauerIDs)
+        return personalInfo.position;
+    }
+
+    leaveRoom(personalInfo){
+        //console.log(this.configRoom.spielerIDs)
+        //console.log(this.configRoom.zuschauerIDs)
+        if(personalInfo.position !== null){
+            //console.log("spieler")
+            for(let i = 0; i < this.configRoom.spielerIDs.length; i++){
+                if(this.configRoom.spielerIDs[i].position === personalInfo.position){
+                    //console.log(i)
+                    this.configRoom.spielerIDs.splice(i, 1);
+                    //console.log(this.configRoom.spielerIDs)
+                    //console.log(this.configRoom.zuschauerIDs)
+                    return 1;
+                }
+            }
+        } else {
+            //console.log("zuschauer")
+            for(let i = 0; i < this.configRoom.zuschauerIDs.length; i++){
+                if(this.configRoom.zuschauerIDs[i].id === personalInfo.id){
+                    //console.log(i)
+                    this.configRoom.zuschauerIDs.splice(i, 1);
+                    //console.log(this.configRoom.spielerIDs)
+                    //console.log(this.configRoom.zuschauerIDs)
+                    return 1;
+                }
+            }
+        }
+
+        return 0;
+    }
+
+    print(){
+        return this.configRoom
+    }
+
+}
+module.exports = Room;
---
+++ Backend/server_projekt/index.js
@@ -0,0 +1,143 @@
+console.log("start server")
+const express = require('express')
+const app = express();
+const http = require("http").createServer();
+const io = require('socket.io')(http, {
+    cors: { origin: "*" },
+});
+const Spiel = require('./GameElements/Spiel');
+const spiel = new Spiel();
+const bodyParser = require("body-parser");
+let Room = require('./GameElements/Room');
+let cors = require('cors');
+const ArrayList = require("arraylist");
+let rooms_list = new ArrayList; //array for all rooms
+
+
+app.use(cors());
+app.use(bodyParser.json())
+app.get('/', function (req, res) {
+    res.send('Hello Worldlul!');
+});
+//create Room
+app.listen(3000, () => console.log("listening on http://localhost:3000"));
+
+//create a new room
+app.post('/room', (req, res) => {
+    console.log("/room")
+    let position = rooms_list.set(req.body.roomname, new Room(req.body));
+    res.status(201).send(position)
+})
+
+//join a room
+app.post('/room/join', (req, res) => {
+    console.log("/room/join")
+    console.log(req.body.roomname)
+    let position = rooms_list.get(req.body.roomname).addPerson(req.body.personalInfo);
+    res.status(200).send(position)
+})
+
+//leave room
+app.post('/room/leave', (req, res) => {
+    console.log("/room/leave")
+    rooms_list.get(req.body.roomname).leaveRoom(req.body.personalInfo)
+    res.status(200).send("room left successfully");
+})
+
+//testprint
+app.post('/room/print', (req, res) => {
+    console.log("/room/print")
+    console.log(rooms_list)
+    res.status(200).send("print")
+})
+
+
+
+
+//io.on('connection', (socket) => {
+//    console.log("Socket: Client connected");
+//    socket.emit("message", "Welcome!");
+//
+//});
+//
+
+//io.on("connection", (socket) => {
+//    console.log("connected")
+//    if (userId.length === 0) {
+//        console.log("full");
+//        socket.emit("forceDisconnect");
+//    } else {
+//        console.log("succesfully connected")
+//
+//        console.log("connected success")
+//        let id = userId.shift();
+//        socketId[id] = socket.id;
+//
+//        socket.emit("id", id);
+//        if (userId.length === 0) {
+//            //when the game can start
+//            let kartenFuerSpieler = [];
+//            karten.neueKarten();
+//            karten.mischen();
+//            for (let i = 0; i < 4; i++) {
+//                isFarbeSelected = false;
+//                isSchalgSelected = false;
+//                kartenFuerSpieler[i] = karten.getKarten(); //getting cards for users
+//                io.to(socketId[i]).emit("karten", kartenFuerSpieler[i]);
+//            }
+//            //select schlag and farbe
+//            io.to(socketId[schlagId]).emit("selectSchlag");
+//            io.to(socketId[farbeId]).emit("selectFarbe");
+//        }
+//        //get schlag und farbe
+//        socket.on("selectedSchlag", (data) => {
+//            spiel.isSchalgSelected = true;
+//            spiel.schlag = data;
+//            if (data === "Welli") spiel.batadu = data;
+//            else spiel.batadu = data.split(" ")[1];
+//            //send to the other typ
+//            if (spiel.isFarbeSelected) {
+//                sendSelection();
+//            }
+//        });
+//        socket.on("selectedFarbe", (data) => {
+//            spiel.farbe = data;
+//            isFarbeSelected = true;
+//            //send to the other typ
+//            if (isSchalgSelected) {
+//                sendSelection();
+//            }
+//        });
+//
+//        function sendSelection() {
+//            io.to(socketId[farbeId]).emit("schlag", schlag);
+//            io.to(socketId[schlagId]).emit("farbe", farbe);
+//            if (farbe !== "Welli") farbe = farbe.split(" ")[0];
+//            else farbe = "Schell";
+//            setBeschter();
+//            canKarteLegen();
+//        }
+//
+//        socket.on("karteGelegt", (karte) => {
+//            console.log(karte);
+//            io.emit("karteGelegt", karte);
+//            if (legenCounter === 1) winningKarte = karte;
+//            else betterCard(karte);
+//            console.log("Winning Karte: " + winningKarte);
+//            canKarteLegen();
+//        });
+//
+//        function canKarteLegen() {
+//            if (legenCounter < 4) {
+//                io.to(socketId[istDran]).emit("karteLegen");
+//                istDran = nextId(istDran);
+//                legenCounter++;
+//            }
+//        }
+//        socket.on("disconnect", () => {
+//            userId.push(id);
+//        });
+//    }
+//});
+
+

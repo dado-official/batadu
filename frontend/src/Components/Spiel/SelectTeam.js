2ef7d775426e160789604b775d85186ccc875d7d
Beniamin Terza <beniaminterza@gmail.com>
2021-03-19 14:24:59 +0100

    feat: team aussuchen responsive

--- frontend/src/Components/Spiel/SelectTeam.js
+++ frontend/src/Components/Spiel/SelectTeam.js
@@ -13,10 +13,9 @@ const SelectTeam = ({ setUrl }) => {
 
     return (
         <div
-            className="flex justify-center items-center flex-col m-auto"
-            style={{ width: "23rem" }}
+            className="flex justify-center items-center flex-col m-auto w-96 max-w-1/9"
         >
-            <img src={TeamIcon} alt="Team" className="h-24 mt-16" />
+            <img src={TeamIcon} alt="Team" className="h-24 mt-4" />
             <h3 className="font-bold text-4xl pt-4">Team aussuchen</h3>
             <p className="text-sm text-gray-600 pt-3 text-center mb-10">
                 Clicken Sie auf das Team, den Sie betreten möchten und dann auf
@@ -42,7 +41,7 @@ const SelectTeam = ({ setUrl }) => {
                 <img src={Arrow} alt="Spielen" className="whiteSVG" />
                 <p>Teilnehmen</p>
             </div>
-            <p className="text-sm pt-8 text-gray-600 mb-16">
+            <p className="text-sm pt-8 text-gray-600 mb-4">
                 Möchten Sie ein anderes Spiel beitreten?{" "}
                 <Link to="/spielen">
                     <span className="font-bold underline text-black">
